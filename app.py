5339b809882414ee08d388de5cd352c30f68d27a
Y.kazuki <kazuki.y2002@gmail.com>
2025-01-12 03:07:46 +0900

    Updated app.py for new feature

--- app.py
+++ app.py
@@ -215,5 +215,6 @@ def translate_texts(texts, source_language, target_language):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
